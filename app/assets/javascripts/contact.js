395a9154ccf0a683826168b8eb0745a2e05a62a7
footprintmedia <andrew@footprintmedia.net>
2013-06-05 00:10:43 +0300

    Update contact.js

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,4 +1,5 @@
 $(function () {
+    // Loads the form in the modal template via AJAX
     $('#contact-inline').submit(function () {
         $('#contact').modal({
                 remote: '/contact/form'
@@ -6,6 +7,7 @@ $(function () {
         return false;
     });
 
+    // Run a few actions when the modal window appears
     $('#contact').on('shown', function () {
         $('.modal-footer .text-error').html('');
         var email = $('#contact-inline input[name="email"]').val();
@@ -15,10 +17,12 @@ $(function () {
         $('.form-wrapper, #contact form :input[type="submit"]').show();
     });
 
+    // Destroy the modal when it closes to prevent 'caching' of stale captcha data
     $('#contact').on('hide', function () {
         $(this).removeData('modal');
     });
 
+    // Handles form submission
     $('#contact form').submit(function () {
         $.ajax({
                 type: 'POST',
@@ -33,6 +37,7 @@ $(function () {
                 error: function (response) {},
                 success: function (response) {
                     $('.modal-footer :input[type="submit"]').removeProp('disabled');
+                    // contact_controller.rb returns 1 for success, error will return 0 with additional JSON fields
                     if (response.success == 1) {
                         $('#contact form .message').text('Your message has been sent. We will be in touch shortly.');
                         $('#contact form :input[type="text"],textarea').val('');
